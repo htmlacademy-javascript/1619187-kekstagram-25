fac5ebf732e1df79a7490d74f8a6488e2dbe2937
Yana <yana.timofeeva.94@mail.ru>
2022-03-31 23:00:18 +0300

    Указала верный путь для загрузки данных

--- js/api.js
+++ js/api.js
@@ -11,7 +11,7 @@ const errorTemplate = document.querySelector('#error')
 
 
 const getData = (onSuccess) => {
-  fetch('https://25.javascript.pages.academy/kekstagram/dat')
+  fetch('https://25.javascript.pages.academy/kekstagram/data')
     .then((response) => response.json())
     .then((similarPictures) => {
       if (similarPictures.length) {
